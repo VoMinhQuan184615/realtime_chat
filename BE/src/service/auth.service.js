e5d830e1ccacb0c5391128de3b9e4ecdaf3f8f05
VoMinhQuan184615 <vominhqun14112004@gmail.com>
2025-11-21 15:22:17 +0700

    feat: add feature for friend controller

--- BE/src/service/auth.service.js
+++ BE/src/service/auth.service.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import User from "../model/User.js";
+import User from "../model/user.js";
 import { MESSAGES } from "../constants/messages.js";
 
 const SECRET_KEY = process.env.SECRET_KEY || "change_this_secret";
